2e39dce177718aee9407231d3bef6b02a4e09248
SamA1430 <samuel.arg14@gmail.com>
2024-07-11 19:51:22 -0400

    Add restriction for password and mail

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 const TodoModel = require("./TodoModel");
 
+const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
 const UserSchema = new mongoose.Schema({
   mail: {
     type: String,
     required: true,
     unique: true,
+    match: mailRegex,
   },
   password: {
     type: String,
--- server/src/routes/registerRoute.js
+++ server/src/routes/registerRoute.js
@@ -4,18 +4,21 @@ const bcrypt = require("bcryptjs");
 module.exports = async (req, res) => {
   try {
     const { mail, password } = req.body;
-    console.log(req.body);
-    console.log(password);
+    if (password.length < 8) {
+      return res.status(400).send({ error: "Le mot de passe est trop court" });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
     const newUser = new UserModel({ mail, password: hashedPassword });
+    const validationError = newUser.validateSync();
+    if (validationError) {
+      return res.status(400).send({ error: validationError.message });
+    }
     await newUser.save();
     res.status(201).send({ message: "L'utilisateur a été créé avec succès" });
   } catch (error) {
+    // Si il y a duplication de mail
     if (error.code === 11000) {
-      return res
-        .status(400)
-        .send({ error: "Le nom d'utilisateur existe déjà" });
+      return res.status(400).send({ error: "Le mail existe déjà" });
     }
     res.status(500).send({ error: error.message });
   }
