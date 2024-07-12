ffdd209910cbe26c44195ec617a580287ac14070
SamA1430 <samuel.arg14@gmail.com>
2024-07-11 20:54:16 -0400

    Add loginRequest

---
+++ client/src/api/loginRequest.js
@@ -0,0 +1,19 @@
+import { API_URL } from "./config";
+export default (mail, password) => {
+  return fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      mail: mail,
+      password: password,
+    }),
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Identification échouée");
+    }
+  });
+};
